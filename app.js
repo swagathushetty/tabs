beccef05a707159bee3941bebddf3480895b6e95
swagath shetty <46868295+swagathushetty@users.noreply.github.com>
2020-06-20 09:54:16 +0530

    init commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const btns=document.querySelectorAll('.tab-btn')
+const about=document.querySelector('.about')
+const articles=document.querySelectorAll('.content')
+
+about.addEventListener('click',(e)=>{
+    console.log(e.target.dataset.id) //only buttons have data-id
+    const id=e.target.dataset.id
+    if(id){
+        //remove active from all  buttons
+        btns.forEach((btn)=>{
+            btn.classList.remove('active')
+            e.target.classList.add('active')
+        })
+        //hide other articles
+        articles.forEach(function(article){
+            article.classList.remove('active')
+        })
+        const element=document.getElementById(id)
+        element.classList.add('active')
+
+    }
+})
